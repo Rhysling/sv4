19740022e43c7960dc0508256ed4a5f59ad60e78
Rhysling <rkummer@polson.com>
2023-09-04 07:19:38 -0700

    Initial.

---
+++ src/components/Counter.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  let count = 0;
+
+  const increment = () => {
+    count += 1
+  };
+
+  const decrement = () => {
+    count -= 1
+  };
+</script>
+
+<p>
+  <button on:click={increment}>
+		Increment
+  </button>
+  
+  <button on:click={decrement}>
+    Decrement
+  </button>
+</p>
+
+<p>
+  Count is {count}.
+</p>
---
+++ src/components/GlobalCss.svelte
@@ -0,0 +1,3 @@
+<style lang="scss" global>
+  @import "../styles/global.scss";
+</style>
